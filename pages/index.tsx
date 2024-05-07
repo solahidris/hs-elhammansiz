953e81a68968d4ff0e41973afc692b056a2268b6
solahidris <emailsolah@gmail.com>
2024-05-07 23:11:29 +0800

    title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 const Home = () => {
   return (
     <main className="flex flex-col items-center justify-center p-6 min-h-screen">
-      <p className="font-semibold text-5xl">if it works</p>
-      <p className="font-semibold text-3xl">it works</p>
+      <p className="font-semibold text-5xl">Elham</p>
+      <p className="font-semibold text-3xl">Mansiz</p>
     </main>
   );
 }
