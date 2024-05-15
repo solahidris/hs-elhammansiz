50c77ccecb16612de85c6e7ee80124db589a77c2
solahidris <emailsolah@gmail.com>
2024-05-15 11:41:54 +0800

    about mobile

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -8,7 +8,7 @@ import { FaHouse as HouseIcon } from "react-icons/fa6";
 
 const Timeline = () => {
   return (
-    <div className='w-full lg:mt-[120px]'>
+    <div className='w-full lg:mt-[120px] mt-[100px] px-8'>
       <VerticalTimeline lineColor="#f1f1ef" >
         <VerticalTimelineElement
           // className="vertical-timeline-element--work"
@@ -20,11 +20,11 @@ const Timeline = () => {
           iconStyle={{ background: '#FFD700', color: '#fff' }}
           icon={<StarIcon />}
         >
-          <h3 className="text-2xl pb-4">Group Director</h3>
+          <h3 className="text-2xl">Group Director</h3>
           {/* <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4> */}
-          <span className='font-thin text-sm'>
+          <p><span className='font-thin text-sm'>
             With New Consultant and Agency Director
-          </span>
+          </span></p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           // className="vertical-timeline-element--work"
@@ -34,11 +34,11 @@ const Timeline = () => {
           iconStyle={{ background: '#899499', color: '#fff' }}
           icon={<HouseIcon />}
         >
-          <h3 className="text-2xl pb-4">Owner KPM</h3>
+          <h3 className="text-2xl">Owner KPM</h3>
           {/* <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4> */}
-          <span className='font-thin text-sm'>
+          <p><span className='font-thin text-sm'>
           Established New Koperasi Perumahan Malaysia
-          </span>
+          </span></p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           // className="vertical-timeline-element--work"
@@ -48,11 +48,11 @@ const Timeline = () => {
           iconStyle={{ background: '#899499', color: '#fff' }}
           icon={<HouseIcon />}
         >
-          <h3 className="text-2xl pb-4">Top Agency Property Partner</h3>
+          <h3 className="text-2xl">Top Agency Property Partner</h3>
           {/* <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4> */}
-          <span className='font-thin text-sm'>
+          <p><span className='font-thin text-sm'>
           In Collaborations with Koperasi Malaysia
-          </span>
+          </span></p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           // className="vertical-timeline-element--work"
@@ -62,11 +62,11 @@ const Timeline = () => {
           iconStyle={{ background: '#899499', color: '#fff' }}
           icon={<HouseIcon />}
         >
-          <h3 className="text-2xl pb-4">Property</h3>
+          <h3 className="text-2xl">Property</h3>
           {/* <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4> */}
-          <span className='font-thin text-sm'>
+          <p><span className='font-thin text-sm'>
           Commenced Property Market Venture
-          </span>
+          </span></p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           // className="vertical-timeline-element--work"
@@ -76,11 +76,11 @@ const Timeline = () => {
           iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
           icon={<WorkIcon />}
         >
-          <h3 className="text-2xl pb-4">Start</h3>
+          <h3 className="text-2xl">Start</h3>
           {/* <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4> */}
-          <span className='font-thin text-sm'>
+          <p><span className='font-thin text-sm'>
           Diverse Business Persuits
-          </span>
+          </span></p>
         </VerticalTimelineElement>
 
       </VerticalTimeline>
--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -70,26 +70,26 @@ const AboutPage = () => {
         </div>
       </div>
 
-      <div id="reviews" className="flex flex-col lg:flex-row">
+      <div id="reviews" className="flex flex-col lg:flex-row mt-[800px] lg:mt-0">
         <span className="absolute right-0 text-[20px] lg:text-5xl z-10 font-bold mt-[8px] lg:mt-[0px] bg-gradient-to-b from-black to-black/70 text-white px-6 py-1 lg:px-10 lg:py-4 rounded-l-lg lg:rounded-l-xl z-0">
           Our Journey
         </span>
         <Timeline />
       </div>
 
-      <div id="projects" className="pt-3 lg:pt-28">
+      <div id="projects" className="pt-3 lg:pt-28 mt-12 lg:mt-0">
         <span
           id="highlights"
           className="absolute left-0 text-[20px] lg:text-5xl z-10 font-bold mt-[15px] lg:mt-[-36px] bg-gradient-to-b from-black to-black/70 text-white px-6 py-1 lg:px-10 lg:py-4 rounded-r-lg lg:rounded-r-xl z-0"
         >
           Vision & Mission
         </span>
-        <div className="w-full lg:w-full flex flex-col gap-10 lg:gap-20 justify-center lg:justify-start mx-auto items-center px-8 lg:px-[180px] mt-[60px] lg:mt-[90px]">
+        <div className="w-full lg:w-full flex flex-col gap-10 lg:gap-20 justify-center lg:justify-start mx-auto items-center px-8 lg:px-[180px] mt-[90px] lg:mt-[90px]">
           <div className="w-full flex items-center">
-          <p className="text-xl font-base w-[80%] leading-8 tracking-wider text-justify">
-            <span className="text-5xl lg:text-7xl font-bold leading-[60px] lg:leading-[120px]">
+          <div className="text-xl font-base w-full lg:w-[80%] leading-8 tracking-wide lg:tracking-wider text-justify">
+            <p className="text-5xl lg:text-7xl font-bold leading-[60px] lg:leading-[120px]">
               Vision
-            </span>
+            </p>
             <br />
             {isEnglish ? (
               <span>
@@ -107,7 +107,7 @@ const AboutPage = () => {
             )}
             <br />
             <br />
-          </p>
+          </div>
           {/* <Image
               src={`/about_hero.png`}
               alt={`carousel_image_1.png`}
@@ -115,7 +115,7 @@ const AboutPage = () => {
               height={1000}
               className="bg-cover rounded-lg aspect-square object-cover w-[30vw]"
             /> */}
-            <div className="w-[50%] lg:pl-10">
+            <div className="w-[30%] lg:w-[50%] lg:pl-10 lg:relative absolute right-10 mt-[-280px] lg:right-0 lg:mt-0">
               <Lottie animationData={animationAboutVision} loop={true} />
             </div>
           </div>
@@ -127,10 +127,11 @@ const AboutPage = () => {
               height={1000}
               className="bg-cover rounded-lg aspect-square object-cover w-[30vw]"
             /> */}
-            <div className="w-[50%] lg:pr-10">
+            {/* <div className="w-[50%] lg:pr-10"> */}
+            <div className="w-[30%] lg:w-[50%] lg:pl-10 lg:relative absolute left-4 mt-[-540px] lg:left-0 lg:mt-0">
               <Lottie animationData={animationAboutMission} loop={true} />
             </div>
-            <div className="text-xl font-base w-[80%] leading-8 tracking-wider text-justify">
+            <div className="text-xl font-base w-full lg:w-[80%] leading-8 tracking-wide lg:tracking-wider text-justify">
               <p className="text-5xl lg:text-7xl font-bold leading-[60px] lg:leading-[60px] text-end">
                 Mission
               </p>
@@ -196,7 +197,7 @@ const AboutPage = () => {
 
       {/* <LayoutGridHomepage /> */}
 
-      <div className="py-[500px] lg:py-60">.</div>
+      <div className="py-[50px] lg:py-40">.</div>
       {/* See Line 55 - Layout-grid.tsx */}
 
       <Footer />
