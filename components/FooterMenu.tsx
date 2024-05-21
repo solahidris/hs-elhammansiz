2c09e6a1754a60f8beb0c4544442cbea32a2d4ca
solahidris <emailsolah@gmail.com>
2024-05-21 23:31:04 +0800

    footer

--- components/FooterMenu.tsx
+++ components/FooterMenu.tsx
@@ -5,12 +5,12 @@ import { Input } from "./ui/input";
 
 const FooterMenu = () => {
 
-    const footerLinkButtonCSS = "hover:text-gray-300 text-white cursor-pointer hover:translate-x-1 transition";
+    const footerLinkButtonCSS = "hover:text-gray-300 text-white cursor-pointer hover:translate-x-1 transition ";
 
     return (
         <div className="flex flex-col gap-10 tracking-[2px] px-6 lg:px-12 pt-12 mt-40 bg-gray-50 text-white"
             style={{ backgroundImage: `url('/blue_background.png')` }}>
-            <div className="grid grid-cols-2 lg:grid-cols-4 text-2xl leading-[36px]">
+            <div className="grid grid-cols-2 lg:grid-cols-4 text-lg lg:text-2xl leading-[36px]">
                 <div className="flex flex-col">
                     <span onClick={() => scrollToSection("header")} className="pb-4 cursor-pointer text-gray-300 hover:text-gray-400">Elham Mansiz</span>
                     <Link href="/about-us" className={`${footerLinkButtonCSS}`}>About us</Link>
@@ -34,20 +34,23 @@ const FooterMenu = () => {
                     <Link href="https://www.tiktok.com/@elhammansiz" className={`${footerLinkButtonCSS}`}>Tiktok</Link>
                     <Link href="https://api.whatsapp.com/send?phone=60167138848&text=Hi%20Elham%20Mansiz!" className={`${footerLinkButtonCSS}`}>Whatsapp</Link>
                 </div>
-                <div className="hidden lg:block flex flex-col gap-4 text-xs">
-                    <span>Have questions?</span>
-                    <Link href="https://api.whatsapp.com/send?phone=60167138848&text=Hi%20Elham%20Mansiz!">
-                        <Button className="rounded-none py-6 bg-sky-300 hover:bg-sky-400 w-full">
-                            Contact us
-                        </Button>
-                    </Link>
-                    <br />
-                    <span>Get updates</span>
-                    <span className="text-gray-200">Drop us your email to learn what&apos;s next.</span>
-                    <Input className="rounded-none py-6 placeholder:text-gray-300" placeholder="aliffaziz@gmail.com" />
+                <div className="hidden lg:block">
+                    <div className="flex flex-col gap-4 text-xs">
+                        <span>Have questions?</span>
+                        <Link href="https://api.whatsapp.com/send?phone=60167138848&text=Hi%20Elham%20Mansiz!">
+                            <Button className="rounded-none py-6 bg-sky-300 hover:bg-sky-400 w-full">
+                                Contact us
+                            </Button>
+                        </Link>
+                        <br />
+                        <span>Get updates</span>
+                        <span className="text-gray-200">Drop us your email to learn what&apos;s next.</span>
+                        <Input className="rounded-none py-6 placeholder:text-gray-300" placeholder="aliffaziz@gmail.com" />
+                    </div>
                 </div>
             </div>
-            <div className="lg:hidden block flex flex-col gap-4 text-xs">
+            <div className="lg:hidden block">
+                <div className="flex flex-col gap-4 text-xs">
                     <span>Have questions?</span>
                     <Link href="https://api.whatsapp.com/send?phone=60167138848&text=Hi%20Elham%20Mansiz!">
                         <Button className="rounded-none py-6 bg-sky-300 hover:bg-sky-400 w-full">
@@ -59,6 +62,7 @@ const FooterMenu = () => {
                     <span className="text-gray-200">Drop us your email to learn what&apos;s next.</span>
                     <Input className="rounded-none py-6 placeholder:text-gray-300" placeholder="aliffaziz@gmail.com" />
                 </div>
+            </div>
             <div className="flex justify-between py-10 lg:text-sm text-xs">
                 <Link href="https://api.whatsapp.com/send?phone=60127710626&text=Hi%20Solah.%20Can%20you%20help%20me%20develop%20a%20website%3F%20I%20would%20like%20to%20know%20more.%20Thanks" className="text-gray-300 font-thin">© 2024 Elham Mansiz - Developed by HireSolah</Link>
                 <div className="flex gap-8 text-black">
